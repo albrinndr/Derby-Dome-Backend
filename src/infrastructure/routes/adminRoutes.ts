94ada59005eeb290b5b5c0585ad3e84e959ad9f9
Albrin <albrinndr@gmail.com>
2023-10-30 00:16:16 +0530

    Admin banner route added

--- src/infrastructure/routes/adminRoutes.ts
+++ src/infrastructure/routes/adminRoutes.ts
@@ -7,16 +7,26 @@ import AdminUseCase from '../../useCase/adminUseCase';
 import JWTToken from '../utils/generateToken';
 import Encrypt from '../utils/bcryptPassword';
 import { protect } from '../middleware/adminAuth';
+import BannerRepository from '../repository/bannerRepository';
+import BannerUseCase from '../../useCase/bannerUseCase';
+import BannerController from '../../adapters/controllers/bannerController';
+import { ImageUpload } from '../config/multer';
+import CloudinaryUpload from '../utils/cloudinaryUpload';
 
 const encrypt = new Encrypt();
 const jwt = new JWTToken();
 const adminRepository = new AdminRepository();
 const userRepository = new UserRepository();
 const clubRepository = new ClubRepository();
+const bannerRepository = new BannerRepository();
+const cloudinary = new CloudinaryUpload();
+
 
 const adminCase = new AdminUseCase(adminRepository, encrypt, jwt, userRepository, clubRepository);
-const controller = new AdminController(adminCase);
+const bannerCase = new BannerUseCase(bannerRepository);
 
+const controller = new AdminController(adminCase);
+const bannerController = new BannerController(bannerCase,cloudinary);
 const router = express.Router();
 
 router.post('/login', (req, res) => controller.login(req, res));
@@ -27,5 +37,8 @@ router.put('/users/action', protect, (req, res) => controller.blockUser(req, res
 
 router.get('/clubs', protect, (req, res) => controller.getClubs(req, res));
 router.put('/clubs/action', protect, (req, res) => controller.blockClub(req, res));
+
+router.get('/banner', protect, (req, res) => bannerController.getBanners(req, res));
+router.put('/banner', protect, ImageUpload.single('image'),(req, res) => bannerController.updateBanner(req, res));
 
 export default router;
