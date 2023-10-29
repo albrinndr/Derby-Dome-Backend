b4f6efd77e96f99d45c69153eaf881d6f107d3f0
Albrin <albrinndr@gmail.com>
2023-10-29 20:12:14 +0530

    Admin protect middleware added

---
+++ src/infrastructure/middleware/adminAuth.ts
@@ -0,0 +1,36 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string;
+            clubId?: string;
+        }
+    }
+}
+
+const protect = async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+
+    token = req.cookies.adminJWT;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+            if (decoded) {
+                next();
+            } else {
+                return res.status(401).json({ message: 'Not authorized, invalid token' });
+            }
+
+        } catch (error) {
+            return res.status(401).json({ message: 'Not authorized, invalid token' });
+        }
+    } else {
+        return res.status(401).json({ message: 'Not authorized, invalid token' });
+    }
+};
+
+export { protect };
--- src/infrastructure/routes/adminRoutes.ts
+++ src/infrastructure/routes/adminRoutes.ts
@@ -6,6 +6,7 @@ import ClubRepository from '../repository/clubRepository';
 import AdminUseCase from '../../useCase/adminUseCase';
 import JWTToken from '../utils/generateToken';
 import Encrypt from '../utils/bcryptPassword';
+import { protect } from '../middleware/adminAuth';
 
 const encrypt = new Encrypt();
 const jwt = new JWTToken();
@@ -21,10 +22,10 @@ const router = express.Router();
 router.post('/login', (req, res) => controller.login(req, res));
 router.post('/logout', (req, res) => controller.logout(req, res));
 
-router.get('/users', (req, res) => controller.getUsers(req, res));
-router.put('/users/action', (req, res) => controller.blockUser(req, res));
+router.get('/users', protect, (req, res) => controller.getUsers(req, res));
+router.put('/users/action', protect, (req, res) => controller.blockUser(req, res));
 
-router.get('/clubs', (req, res) => controller.getClubs(req, res));
-router.put('/clubs/action', (req, res) => controller.blockClub(req, res));
+router.get('/clubs', protect, (req, res) => controller.getClubs(req, res));
+router.put('/clubs/action', protect, (req, res) => controller.blockClub(req, res));
 
 export default router;
