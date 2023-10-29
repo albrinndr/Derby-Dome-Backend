15eb20ea76bd0c94662144d5219a5f8717ccecf3
Albrin <albrinndr@gmail.com>
2023-10-30 00:16:02 +0530

    Home screen banner access route added

--- src/infrastructure/routes/userRoutes.ts
+++ src/infrastructure/routes/userRoutes.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-
 import UserController from '../../adapters/controllers/userController';
 import UserUseCase from '../../useCase/userUseCase';
 import UserRepository from '../repository/userRepository';
@@ -8,14 +7,26 @@ import JWTToken from '../utils/generateToken';
 import GenerateEmail from '../utils/sendMail';
 import GenerateOtp from '../utils/generateOtp';
 import { protect } from '../middleware/userAuth';
+import BannerRepository from '../repository/bannerRepository';
+import BannerUseCase from '../../useCase/bannerUseCase';
+import BannerController from '../../adapters/controllers/bannerController';
+import CloudinaryUpload from '../utils/cloudinaryUpload';
+
 
 const repository = new UserRepository();
 const encrypt = new Encrypt();
 const jwt = new JWTToken();
 const otp = new GenerateOtp();
 const email = new GenerateEmail();
+const bannerRepository = new BannerRepository();
+const cloudinary = new CloudinaryUpload();
+
 const userCase = new UserUseCase(repository, encrypt, jwt);
+const bannerCase = new BannerUseCase(bannerRepository);
+
 const controller = new UserController(userCase, email, otp);
+const bannerController = new BannerController(bannerCase, cloudinary);
+
 
 const router = express.Router();
 
@@ -27,5 +38,7 @@ router.post('/logout', (req, res) => controller.logout(req, res));
 
 router.get('/profile', protect, (req, res) => controller.profile(req, res));
 router.put('/profile', protect, (req, res) => controller.updateProfile(req, res));
+
+router.get('/banner', (req, res) => bannerController.getBanners(req,res));
 
 export default router;
