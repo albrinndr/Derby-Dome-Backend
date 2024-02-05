40ffe731193ed513596ba8a184479ce4c13e8934
Albrin <albrinndr@gmail.com>
2024-02-05 12:56:35 +0530

    Pm2 config

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { httpServer } from "./infrastructure/config/app";
-import { connectDB } from "./infrastructure/config/db";
+import { httpServer } from "./src/infrastructure/config/app";
+import { connectDB } from "./src/infrastructure/config/db";
 
 const PORT = process.env.PORT || 8000;
 
